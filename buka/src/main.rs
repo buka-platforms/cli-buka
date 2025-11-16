998185695c50c003f2afab5fb258dd1c9dbeb222
Sony AK <sony@sony-ak.com>
2025-11-16 13:05:30 +0700

    Improve error messages for plugin library loading and function export

--- buka/src/main.rs
+++ buka/src/main.rs
@@ -33,10 +33,10 @@ fn main() {
     if let Some(plugin) = cli.plugin {
         let lib_path = format!("dist_plugins/{}", get_library_filename(&plugin));
         unsafe {
-            let lib = Library::new(lib_path).expect("❌ Failed to load plugin library.");
+            let lib = Library::new(lib_path).expect("Error: Failed to load plugin library.");
             let func: Symbol<unsafe extern "C" fn(*const std::os::raw::c_char)> = lib
                 .get(b"run_plugin")
-                .expect("❌ Plugin does not export function run_plugin");
+                .expect("Error: Plugin does not export function run_plugin");
             let json = serde_json::to_string(&cli.args).unwrap();
             let c_args = CString::new(json).unwrap();
             func(c_args.as_ptr());
